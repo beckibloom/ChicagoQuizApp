e212258fc41655c8006701ebe42e92aa8e1f2a76
Rebecca Blumer <r.blumer@fw.local>
2019-04-20 14:19:56 -0500

    added scoring functionality

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const STORE = [
     answer3: 'Peppers',
     answer4: 'Ketchup',
     correctAnswer: 'Ketchup',
-    successtext: 'If you put ketchup on your hot dog, prepare to be the laughing stock of Wrigley Field!',
+    successtext: 'If you put ketchup on your hot dog, prepare to be the laughing stock of Wrigley Field.',
     successimage: '<img src="https://s3.amazonaws.com/cdn.tastesofchicago.com/images/uploads/category_954_7648.jpg" alt="Two delicious Chicago-style hot dogs" class="success-image">'},
 
     // 1
@@ -20,7 +20,7 @@ const STORE = [
     answer4: 'Uno\'s',
     correctAnswer: 'Lou Malnati\'s',
     successtext: 'Lou Malnati\'s is objectively the best, and anyone saying otherwise is probably not a true Chicagoan.',
-    successimage: '<img src="https://scontent-ort2-1.xx.fbcdn.net/v/t31.0-8/1421050_10152086204949828_1243982359_o.jpg?_nc_cat=107&_nc_ht=scontent-ort2-1.xx&oh=050e3c99392ca998554ea9439018b46f&oe=5D32971F" alt="A cheesy tomato deep dish pizza, fresh out of the oven">'},
+    successimage: '<img src="https://scontent-ort2-1.xx.fbcdn.net/v/t31.0-8/1421050_10152086204949828_1243982359_o.jpg?_nc_cat=107&_nc_ht=scontent-ort2-1.xx&oh=050e3c99392ca998554ea9439018b46f&oe=5D32971F" alt="A cheesy tomato deep dish pizza, fresh out of the oven" class="success-image">'},
 
     // 2
     {question: 'Question 3: When you\'re craving some good curry, what street should you head toward?',
@@ -30,7 +30,7 @@ const STORE = [
     answer4: 'Damen',
     correctAnswer: 'Devon',
     successtext: 'Devon Street is known for it\'s many amazing and authentic Indian restaurants.',
-    successimage: '<img src="https://assets3.thrillist.com/v1/image/1296125/size/tl-horizontal_main_2x.jpg" alt="Plate with butter chicken, rice, chickpeas, and naan bread.">'},
+    successimage: '<img src="https://assets3.thrillist.com/v1/image/1296125/size/tl-horizontal_main_2x.jpg" alt="Plate with butter chicken, rice, chickpeas, and naan bread." class="success-image">'},
 
     // 3
     {question: 'Question 4: You\'re heading out for a night on the town, and looking for a neighborhood full of parties and <span class="PRIDE">PRIDE</span>. Where is your Lyft dropping you off?',
@@ -40,7 +40,7 @@ const STORE = [
     answer4: 'The Sixth',
     correctAnswer: 'Roscoe\'s',
     successtext: 'Roscoe\'s is a great first stop for a fun night in Boystown on North Halsted Street. Boystown is a hot spot filled with fun bars for karaoke, drag shows, and dancing the night away!',
-    successimage: '<img src="https://www.wfmt.com/wp-content/uploads/2018/07/Boystown_RainbowPylons.jpg" alt="Rainbow pride flags on display with rainbow signpost with signage for Northalsted in Boystown">'},
+    successimage: '<img src="https://www.wfmt.com/wp-content/uploads/2018/07/Boystown_RainbowPylons.jpg" alt="Rainbow pride flags on display with rainbow signpost with signage for Northalsted in Boystown" class="success-image">'},
 
     // 4
     {question: 'Question 5: It\'s time for some dim sum. Which of these trains will take you directly to Chinatown?',
@@ -50,7 +50,7 @@ const STORE = [
     answer4:'Red line',
     correctAnswer: 'Red line',
     successtext: 'The Red Line stops right in the heart of Chinatown for all of your dim sum and bubble tea needs. Yum!',
-    successimage: '<img src="https://www.transitchicago.com/assets/1/6/ctamap_Lsystem.png" alt="Map of the L train system, from Chicago Transit Authority">'},
+    successimage: '<img src="https://www.transitchicago.com/assets/1/6/ctamap_Lsystem.png" alt="Map of the L train system, from Chicago Transit Authority" class="success-image">'},
 
     // 5 
     {question: 'Question 6: They serve some of the best Italian beef sandwiches in the land, but their owner, Dick, can be kind of a... you-know-what. You\'re on your way to:',
@@ -60,7 +60,7 @@ const STORE = [
     answer4:'Al\'s Beef',
     correctAnswer: 'Portillo\'s',
     successtext: 'Portillo\'s is a Chicago staple, and the classic Italian beef sandwich comes with either sweet peppers or hot giardiniera, if you\'re feeling spicy.',
-    successimage: '<img src="https://tribwxin.files.wordpress.com/2017/06/photo-credit-portillos-via-facebook.jpg?quality=85&strip=all&w=1200" alt="Italian beef sandwich with hot giardiniera and fries from Portillo\'s.">'},
+    successimage: '<img src="https://tribwxin.files.wordpress.com/2017/06/photo-credit-portillos-via-facebook.jpg?quality=85&strip=all&w=1200" alt="Italian beef sandwich with hot giardiniera and fries from Portillo\'s." class="success-image">'},
 
     // 6
     {question: 'Question 7: Two massive lions have stood guard outside this building since the 1893 Chicago World\'s Fair. What is this building called?',
@@ -69,8 +69,8 @@ const STORE = [
     answer3: 'The Chicago History Museum',
     answer4: 'The Richard H. Driehaus Museum',
     correctAnswer: 'The Art Institute of Chicago',
-    successtext: 'The two iconic bronze lion statues outside of the Art Institute of Chicago weigh over two tons each! The one positioned north of the steps is "on the prowl" and the lion to the south is "in an attitude of defiance," according to sculptor Edward Kemeys.',
-    successimage: '<img src="https://gcottraux.files.wordpress.com/2015/10/south-lion.jpg" alt="The south lion from the steps of The Art Institute of Chicago">'},
+    successtext: 'The two iconic bronze lion statues stand outside of the Art Institute of Chicago, and they weigh over two tons each! The one positioned north of the steps is "on the prowl" and the lion to the south is "in an attitude of defiance," according to sculptor Edward Kemeys.',
+    successimage: '<img src="https://gcottraux.files.wordpress.com/2015/10/south-lion.jpg" alt="The south lion from the steps of The Art Institute of Chicago" class="success-image">'},
 
     // 7
     {question: 'Question 8: You\'re making your way downtown, walking fast, faces pass, and... taking some friends to see the bean! What\'s the closest attraction you could also show your friends, while they\'re visiting from out of town?',
@@ -80,7 +80,7 @@ const STORE = [
     answer4:'The Sears Tower',
     correctAnswer: 'Maggie Daley Park',
     successtext: 'Maggie Daley Park has got something fun for every season, and it\'s right near the bean in the heart of The Loop (our downtown area). Go rock climbing in the summer, ice skating in the winter, or play in the playground year-round!',
-    successimage: '<img src="https://maggiedaleypark.com/maggiedaley/wp-content/uploads/2016/10/MaggieDaley1_%C2%A9LupuIonut-Flickr1.jpg" alt="An overhead view of Maggie Daley Park at sunset.">'},
+    successimage: '<img src="https://maggiedaleypark.com/maggiedaley/wp-content/uploads/2016/10/MaggieDaley1_%C2%A9LupuIonut-Flickr1.jpg" alt="An overhead view of Maggie Daley Park at sunset." class="success-image">'},
 
     // 8 
     {question: 'Question 9: Much to everyone\'s dismay, the Sears Tower was actually renamed to what?',
@@ -89,8 +89,8 @@ const STORE = [
     answer3:'Aon Center',
     answer4:'Willis Tower',
     correctAnswer: 'Willis Tower',
-    successtext: 'The Willis Tower was renamed in 2009 after the Willis Group Holdings leased the naming rights... but nobody likes calling it that. Just ask the 50,000 people who signed the online petition at ItsTheSearsTower.com.',
-    successimage: '<img src="https://carltongroup.com/wp-content/uploads/2015/08/willis-tower-chicago.jpg" alt="The Sears Tower (Willis Tower) illuminated at night.">'},
+    successtext: 'The Willis Tower was renamed in 2009 after the Willis Group Holdings leased the naming rights... but nobody likes calling it that. Just ask the 50,000 people who signed the online petition at <a href="www.itsthesearstower.com">ItsTheSearsTower.com</a>.',
+    successimage: '<img src="https://carltongroup.com/wp-content/uploads/2015/08/willis-tower-chicago.jpg" alt="The Sears Tower (Willis Tower) illuminated at night." class="success-image">'},
 
     // 9
     {question: 'Question 10: How many seasons does Chicago truly have?',
@@ -100,11 +100,10 @@ const STORE = [
     answer4:'11: Winter, fool\'s spring, second winter, spring of deception, third winter, mud season, actual spring, summer, false fall, second summer (one week), actual fall',
     correctAnswer: '11: Winter, fool\'s spring, second winter, spring of deception, third winter, mud season, actual spring, summer, false fall, second summer (one week), actual fall',
     successtext: 'It\'s 11 seasons! Welcome to Chicago - if you don\'t like the weather, just wait 10 minutes! Sure, lots of people say that about their city, but they\'ve got nothing on us.',
-    successimage: '<img src="https://memegenerator.net/img/instances/75901504/mother-nature-you-cannot-have-all-four-seasons-in-one-week-chicago-hold-my-beer.jpg" alt="Picture of Chicago with text: Mother Nature: You cannot have all four seasons in one week. Chicago: Hold my beer">'}
+    successimage: '<img src="https://memegenerator.net/img/instances/75901504/mother-nature-you-cannot-have-all-four-seasons-in-one-week-chicago-hold-my-beer.jpg" alt="Picture of Chicago with text: Mother Nature: You cannot have all four seasons in one week. Chicago: Hold my beer" class="success-image">'}
 ]
 
 function renderChicagoQuiz(questionNumber) {
-    console.log('`renderChicagoQuiz` ran');
     $('.start-quiz').click(function(event){
         renderQuizQuestion(questionNumber);
         $(this).closest('div').addClass('hidden');
@@ -114,31 +113,26 @@ function renderChicagoQuiz(questionNumber) {
         renderQuizQuestion(questionNumber);
         submitQuizAnswer(questionNumber);
     });
-    // $('.quizForm').on('click', '.next-question', function(event) {
-    //     event.preventDefault();
-    //     console.log('Question submit button was clicked.');
-    //     number +=1;
-    //     renderQuizQuestion(number);
-    // });
 }
 
-function currentQuizQuestion() {
-    console.log('`currentQuizQuestion` ran');
+function startQuiz() {
     let questionNumber = $('.question-number').text();
     questionNumber = Number(questionNumber);
     renderChicagoQuiz(questionNumber);
-    // console.log(`Inside currentQuizQuestion, questionNumber is ${questionNumber}`);
 }
 
 function renderQuizQuestion(questionNumber) {
-    console.log('`renderQuizQuestion` ran');
     const quizQuestionString = generateQuizQuestion(STORE[questionNumber]);  
     $('.quizForm').html(quizQuestionString); 
     $('.question-number').text(questionNumber+1);
 }
 
 function generateQuizQuestion(item) {
-    console.log('`generateQuizQuestion` ran');
+    console.log()
+    //     if (questionNumber < STORE.length) {}
+    // else {
+    //     renderQuizCompleted();
+    // }
     return `
     <div class="quiz-question">
       <form>
@@ -155,9 +149,10 @@ function generateQuizQuestion(item) {
               <input type="radio" name="question${item}" value="${item.answer3}" required>${item.answer3} 
             </li>
             <li>
-              <input type="radio" name="question${item}" value="${item.answer4}"required>${item.answer4} 
+              <input type="radio" name="question${item}" value="${item.answer4}" required>${item.answer4} 
             </li>
           </ul>
+          <span class="error-text"></span>
           <button type="submit" value="Submit" class="submit-answer">Submit</button>
         </fieldset>
       </form>
@@ -175,10 +170,17 @@ function submitQuizAnswer(questionNumber) {
 function evaluateAnswer(answerVal, questionNumber) {
     if (answerVal === STORE[questionNumber].correctAnswer) {
         userFeedbackCorrect(STORE[questionNumber]);
-        renderChicagoQuiz(questionNumber);
+        renderChicagoQuiz(questionNumber+1);
+        let score = $('.score').text();
+        score = Number(score);
+        renderCurrentScore(score+1);
+    }
+    else if (answerVal === undefined) {
+        $('.error-text').text('You must select one of the options above.')
     }
     else {
-        userFeedbackWrong();
+        userFeedbackWrong(STORE[questionNumber]);
+        renderChicagoQuiz(questionNumber+1);
     }
 }
 
@@ -192,40 +194,43 @@ function userFeedbackCorrect(item) {
     </div>`);
 }
 
-function userFeedbackWrong() {
-    console.log('userFeedbackWrong ran.')
+function userFeedbackWrong(item) {
+    $('.quizForm').html(`
+    <div class="user-feedback">
+    <h2>Wrong!</h2>
+    <div class="user-feedback-text">${item.successtext}</div>
+    ${item.successimage}
+    <button type="button" class="next-question">Continue</button>
+    </div>`);
 }
 
-function renderAnswerSuccessPage() {
-    // 7 - Upon submitting an answer, users should:
-    // receive textual feedback about their answer. If they were incorrect, they should be told the correct answer.
-    // be moved onto the next question (or interact with an element to move on).
-    console.log('`renderAnswerSuccessPage` ran');
-}
-
-function updateCurrentScore() {
-    // 6 - Users should also be able to see which question they're on 
-    // (for instance, "7 out of 10") and their current score ("5 correct, 2 incorrect").
-    console.log('`updateCurrentScore` ran');
-}
-
-function renderCurrentScore() {
-    // Responsible for showing the updated amount of updateCurrentScore
-    console.log('`renderCurrentScore` ran');
+function renderCurrentScore(score) {
+    $('.score').text(`${score}`);
 }
 
 function renderQuizCompleted() {
-    // 8 - Users should be shown their overall score at the end of the quiz. In other words, how many questions they got right out of the total questions asked.
-    console.log('`renderQuizCompleted` ran');
+    let score = $('.score').text()
+    $('.quiz-form').html(`
+    <div class="results">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Flag_of_Chicago%2C_Illinois.svg/255px-Flag_of_Chicago%2C_Illinois.svg.png" 
+    alt="The Chicago flag" class="success-image">
+    <h2>You scored</h2>
+    <div class="current-score">${score} out of 10 points!</div>
+    <button class="restart-quiz" type="submit">Take the quiz again!</button>
+    </form>
+    </div>`);
+    restartQuiz();
 }
 
 function restartQuiz() {
-    // 9 - Users should be able to start a new quiz.
-    console.log('`restartQuiz` ran');
+    $('.restart-quiz').click(function(event){
+        location.reload();
+    })
 }
 
 function handleChicagoQuiz() {
-    currentQuizQuestion();
+    startQuiz();
+    renderCurrentScore(0);
 }
 
 $(handleChicagoQuiz);
